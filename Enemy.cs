e058b287fdee441bee3448396f0df107647d0861
316968 <36234189+316968@users.noreply.github.com>
2018-05-17 15:03:07 -0400

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -22,11 +22,17 @@ namespace Rogue_II
         Point enemyPos = new Point(x:1, y:1);
         //the enemyRectangle is where the sprite will be, and there will be collision with it
         public Rectangle enemyRectangle = new Rectangle();
+        //the minibossRectangle is where the mini boss sprite will be.
+        public Rectangle minibossRectangle = new Rectangle();
         //these ints are for enemy stats
         int hp;
+        int bossHP;
         int maxHP;
+        int bossMaxHP;
         int strength;
+        int bossStrength;
         int armour;
+        int bossArmour;
         ImageBrush stormtSprite = new ImageBrush(new BitmapImage(new Uri("H:/My Documents/ICS3U/Unit 4/Rogue II/Rogue II/StormtrooperForward.png")));
         //higher level, more exp for player
         int level;
@@ -40,8 +46,11 @@ namespace Rogue_II
             window = w;
             //System.IO.StreamReader streamReader = new System.IO.StreamReader("map" + levelProgress + ".txt");
             enemyRectangle = new Rectangle();
-            enemyRectangle.Height = 100;
-            enemyRectangle.Width = 100;  
+            minibossRectangle = new Rectangle();
+            enemyRectangle.Height = 30;
+            enemyRectangle.Width = 30;
+            minibossRectangle.Height = 30;
+            minibossRectangle.Width = 30;
             if (levelProgress == 1)
             {   //stormtrooper hoth
                 enemyRectangle.Fill = new ImageBrush(new BitmapImage(new Uri("StormtrooperForward.png", UriKind.Relative)));
@@ -50,11 +59,50 @@ namespace Rogue_II
                 maxHP = 10;
                 strength = 12;
                 armour = 3;
-                int level = 1;
-                canvas.Children.Add(this.enemyRectangle);
-                
+                level = 1;
+                canvas.Children.Add(enemyRectangle);
+                enemyRectangle.Visibility = Visibility.Hidden;
+                // wampa?
+                //minibossRectangle.Fill = new ImageBrush(new BitmapImage(new Uri("wampa.png", UriKind.Relative)));
+                minibossRectangle.Fill = Brushes.Red;
+                Canvas.SetLeft(minibossRectangle, 150);
+                bossHP = 18;
+                bossMaxHP = 18;
+                bossStrength = 17;
+                bossArmour = 1;
+                level = 5;
+                canvas.Children.Add(minibossRectangle);
+                minibossRectangle.Visibility = Visibility.Hidden;
             }
+            if (levelProgress == 2)
+            {
+                // second generic enemy
+                enemyRectangle.Fill = Brushes.Blue;
+                Canvas.SetLeft(enemyRectangle, 100);
+                hp = 22;
+                maxHP = 22;
+                strength = 22;
+                armour = 6;
+                level = 5;
+                canvas.Children.Add(enemyRectangle);
+                enemyRectangle.Visibility = Visibility.Hidden;
+                // second miniboss
+                minibossRectangle.Fill = Brushes.Red;
+                Canvas.SetLeft(minibossRectangle, 150);
+                bossHP = 30;
+                bossMaxHP = 30;
+                bossStrength = 20;
+                bossArmour = 11;
+                level = 10;
+                canvas.Children.Add(minibossRectangle);
+                minibossRectangle.Visibility = Visibility.Hidden;
+            
+            }
+            if (levelProgress == 3)
+            {
+                //third generic enemy
 
+            }
             
         }
         
@@ -66,6 +114,7 @@ namespace Rogue_II
         public void death()
         {
             canvas.Children.Remove(enemyRectangle);
+            player.XP = player.XP + (level * 10);
         }
     }
 }
